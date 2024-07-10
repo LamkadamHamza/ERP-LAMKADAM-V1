d601e75609d4bbdbaa7d5a3acf664827acb7ccd2
hamza lamkadam <hamza.lamkadam@gmail.com>
2024-07-10 14:32:51 +0100

    mon comite

---
+++ Fournisseur-service/src/main/java/com/lamkadam/fournisseurservice/web/FournisseurController.java
@@ -0,0 +1,27 @@
+package com.lamkadam.fournisseurservice.web;
+
+import com.lamkadam.fournisseurservice.entities.Fournisseur;
+import com.lamkadam.fournisseurservice.repository.FournisseurRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class FournisseurController {
+
+    private FournisseurRepository fournisseurRepository;
+
+    public FournisseurController(FournisseurRepository fournisseurRepository) {
+        this.fournisseurRepository = fournisseurRepository;
+    }
+
+
+    @GetMapping("/fournisseurs")
+    public List<Fournisseur> getAllFourniseur(){
+        List<Fournisseur> fournisseurs = fournisseurRepository.findAll();
+        return fournisseurs;
+    }
+}
